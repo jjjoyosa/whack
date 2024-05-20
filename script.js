3237456b2518d85947dac918290a8f29c0774a1d
Jenzzo Joyosa <jjjoyosa@up.edu.ph>
2024-05-20 08:08:13 +0800

    updated

--- script.js
+++ script.js
@@ -2,65 +2,74 @@ let currMoleTile;
 let currPlantTile;
 let score = 0;
 let gameOver = false;
+let moleInterval;
+let plantInterval;
+let pause = false;
 
 window.onload = function() {
     setGame();
 }
 
 function setGame() {
-    //set up the grid in html
-    for (let i = 0; i < 9; i++) { //i goes from 0 to 8, stops at 9
-        //<div id="0-8"></div>
+    // Set up the grid in HTML
+    for (let i = 0; i < 9; i++) {
         let tile = document.createElement("div");
         tile.id = i.toString();
         tile.addEventListener("click", selectTile);
         document.getElementById("board").appendChild(tile);
     }
-    setInterval(setMole, 600); // 1000 miliseconds = 1 second, every 1 second call setMole
-    setInterval(setPlant, 1000); // 2000 miliseconds = 2 seconds, every 2 second call setPlant
+    startGameTimers();
+}
+
+function startGameTimers() {
+    moleInterval = setInterval(setMole, 600);
+    plantInterval = setInterval(setPlant, 1000);
+}
+
+function stopGameTimers() {
+    clearInterval(moleInterval);
+    clearInterval(plantInterval);
 }
 
 function getRandomTile() {
-    //math.random --> 0-1 --> (0-1) * 9 = (0-9) --> round down to (0-8) integers
     let num = Math.floor(Math.random() * 9);
     return num.toString();
 }
 
 function setMole() {
-    if (gameOver) {
-        return;
-    }
-    if (currMoleTile) {
-        currMoleTile.innerHTML = "";
-    }
-    let mole = document.createElement("img");
-    mole.src = "./monty-mole.png";
-
-    let num = getRandomTile();
-    if (currPlantTile && currPlantTile.id == num) {
-        return;
-    }
-    currMoleTile = document.getElementById(num);
-    currMoleTile.appendChild(mole);
+    if (!pause && !gameOver) {
+        if (currMoleTile) {
+            currMoleTile.innerHTML = "";
+        }
+        let mole = document.createElement("img");
+        mole.src = "./cat.png";
+        mole.style.width = "125px";
     
+        let num = getRandomTile();
+        if (currPlantTile && currPlantTile.id == num) {
+            return;
+        }
+        currMoleTile = document.getElementById(num);
+        currMoleTile.appendChild(mole);
+    }
 }
 
 function setPlant() {
-    if (gameOver) {
-        return;
+    if (!pause && !gameOver) {
+        if (currPlantTile) {
+            currPlantTile.innerHTML = "";
+        }
+        let plant = document.createElement("img");
+        plant.src = "./dog.png";
+        plant.style.width = "170px";
+    
+        let num = getRandomTile();
+        if (currMoleTile && currMoleTile.id == num) {
+            return;
+        }
+        currPlantTile = document.getElementById(num);
+        currPlantTile.appendChild(plant);
     }
-    if (currPlantTile) {
-        currPlantTile.innerHTML = "";
-    }
-    let plant = document.createElement("img");
-    plant.src = "./piranha-plant.png";
-
-    let num = getRandomTile();
-    if (currMoleTile && currMoleTile.id == num) {
-        return;
-    }
-    currPlantTile = document.getElementById(num);
-    currPlantTile.appendChild(plant);
 }
 
 function selectTile() {
@@ -68,13 +77,47 @@ function selectTile() {
         return;
     }
     if (this == currMoleTile) {
-        // If the clicked tile contains the mole, remove the mole from that tile
         currMoleTile.innerHTML = "";
         score += 10;
         document.getElementById("score").innerText = score.toString();
     } else if (this == currPlantTile) {
-        // If the clicked tile contains the plant, end the game
-        document.getElementById("score").innerText = "GAME OVER: " + score.toString();
+        let finalScore = score.toString();
+        let gameOverMessage = document.getElementById("gameOverMessage");
+        gameOverMessage.innerText = "GAME OVER!" + "\nScore: " + finalScore + "\n Click this message to start a new game";
+        gameOverMessage.style.display = "block";
+        gameOverMessage.addEventListener("click", function() {
+            reloadPage(); 
+        });
         gameOver = true;
     }
 }
+
+function reloadPage() {
+    window.location.reload(); // Reload the page
+}
+
+
+function togglePause() {
+    pause = !pause;
+    let pauseMessage = document.getElementById("pauseMessage");
+    if (pause) {
+        stopGameTimers();
+        pauseMessage.style.display = "block";
+        document.getElementById("pauseBtn").innerText = "Resume";
+    } else {
+        startGameTimers();
+        pauseMessage.style.display = "none";
+        document.getElementById("pauseBtn").innerText = "Pause";
+    }
+}
+
+function displayInstructions() {
+    let instructionsMessage = document.getElementById("instructionsMessage");
+    if (instructionsMessage.style.display === "block") {
+        instructionsMessage.style.display = "none";
+    } else {
+        instructionsMessage.style.display = "block";
+    }
+}
+
+
